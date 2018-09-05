ca940c8c53019c25bb7973ae7c151768646dbbd3
craig <cbrookes@redhat.com>
2018-09-05 17:33:54 +0100

    more scaffold

--- pkg/integration/reconcile.go
+++ pkg/integration/reconcile.go
@@ -3,13 +3,19 @@ package integration
 import (
 	"context"
 
+	controllerErr "github.com/integr8ly/integration-controller/pkg/errors"
+
+	"github.com/pkg/errors"
+
 	"github.com/integr8ly/integration-controller/pkg/apis/integration/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-type Reconciler struct{}
+type Reconciler struct {
+	fuse *Fuse
+}
 
 func (h *Reconciler) GVK() schema.GroupVersionKind {
 	return schema.GroupVersionKind{
@@ -20,6 +26,37 @@ func (h *Reconciler) GVK() schema.GroupVersionKind {
 }
 
 func (h *Reconciler) Handle(ctx context.Context, event sdk.Event) error {
-	logrus.Info("handling object ", event.Object.GetObjectKind().GroupVersionKind().String())
+	logrus.Info("handling integration ")
+	integration, ok := event.Object.(*v1alpha1.Integration)
+	if !ok {
+		return errors.New("expected a integration object but got " + event.Object.GetObjectKind().GroupVersionKind().String())
+	}
+	if integration.Status.Phase == v1alpha1.PhaseNone {
+		integration, err := h.Accept(ctx, integration)
+		if controllerErr.IsNotEnabledErr(err) {
+			logrus.Debug("integration is not enabled ", integration.Name, "doing nothing")
+			return nil
+		}
+		if err != nil {
+			return errors.Wrap(err, "failed to accept new integration")
+		}
+
+		return sdk.Update(integration)
+	}
+	switch integration.Spec.Service {
+	case "fuse":
+		return h.fuse.Integrate(ctx, integration)
+	default:
+		return errors.New("unknown integration type " + integration.Spec.Service)
+	}
 	return nil
+}
+
+func (h *Reconciler) Accept(ctx context.Context, integration *v1alpha1.Integration) (*v1alpha1.Integration, error) {
+	ic := integration.DeepCopy()
+	if !ic.Spec.Enabled {
+		return nil, &controllerErr.NotEnabledErr{}
+	}
+	ic.Status.Phase = v1alpha1.PhaseAccepted
+	return ic, nil
 }
