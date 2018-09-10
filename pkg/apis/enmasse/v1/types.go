56e0636eb4fe28d5ca090b3e093ebd0ba2a05e61
John Roche <jroche@redhat.com>
2018-09-10 11:07:02 +0100

    Add create integration functionality from enmasse namespace

--- pkg/apis/enmasse/v1/types.go
+++ pkg/apis/enmasse/v1/types.go
@@ -10,11 +10,31 @@ type AddressSpace struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
 	Spec              AddressSpaceSpec `json:"spec"`
+	Status            AddressSpaceStatus `json:"status"`
 }
 
 type AddressSpaceSpec struct {
 	Type string `json:"type"`
 	Plan string `json:"plan"`
+}
+
+type AddressSpaceStatus struct {
+	IsReady bool `json:"isReady"`
+	EndPointStatuses []EndPointStatus `json:"endpointStatuses"`
+}
+
+type EndPointStatus struct {
+	metav1.ObjectMeta `json:"metadata"`
+	Name string `json:"name"`
+	ServiceHost string `json:"serviceHost"`
+	ServicePorts []ServicePort `json:"servicePorts"`
+	Host string `json:"host"`
+	Port int `json:"port"`
+}
+
+type ServicePort struct {
+	Name string `json:"name"`
+	Port int `json:"port"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
--- pkg/enmasse/reconcile.go
+++ pkg/enmasse/reconcile.go
@@ -2,14 +2,23 @@ package enmasse
 
 import (
 	"context"
-
+	"fmt"
+	"github.com/integr8ly/integration-controller/pkg/apis/enmasse/v1"
+	integration "github.com/integr8ly/integration-controller/pkg/apis/integration/v1alpha1"
+	syndesis "github.com/integr8ly/integration-controller/pkg/apis/syndesis/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	"github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	v12 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"encoding/json"
 )
 
 type Reconciler struct {
 }
+
+var integrationType = "amqp"
 
 func (r *Reconciler) GVK() schema.GroupVersionKind {
 	return schema.GroupVersionKind{
@@ -20,9 +29,79 @@ func (r *Reconciler) GVK() schema.GroupVersionKind {
 
 }
 
+func syndesisExists(namespace string) (bool, error) {
+	syndesisList := syndesis.NewSyndesisList()
+	if err := sdk.List(namespace, syndesisList); err != nil {
+		return false, errors.New("Failed to list syndesis custom resources")
+	}
+
+	return len(syndesisList.Items) > 0, nil
+}
+
+func unMarshalAddressSpace(cfgm *v12.ConfigMap) (*v1.AddressSpace, error) {
+	addressSpaceData := cfgm.Data["config.json"]
+
+	var addressSpace v1.AddressSpace
+	err := json.Unmarshal([]byte(addressSpaceData), &addressSpace)
+	return &addressSpace, err
+}
+
+func createIntegrationResource(as *v1.AddressSpace, namespace string) *integration.Integration {
+	ingrtn := integration.NewIntegration()
+	for _, endPointStatus := range as.Status.EndPointStatuses {
+		if endPointStatus.Name == "messaging" {
+			ingrtn := integration.NewIntegration()
+			ingrtn.ObjectMeta.Name = as.Name
+			ingrtn.ObjectMeta.Namespace = namespace
+			ingrtn.Spec.MessagingHost = endPointStatus.ServiceHost + ":" + fmt.Sprintf("%d", endPointStatus.Port)
+			ingrtn.Spec.Realm = as.Annotations["enmasse.io/realm-name"]
+			ingrtn.Spec.IntegrationType = integrationType
+			ingrtn.Spec.Service = "fuse"
+			ingrtn.Spec.Enabled = false
+
+			for _, servicePort := range endPointStatus.ServicePorts {
+				if servicePort.Name == integrationType {
+					ingrtn.Spec.MessagingHost = endPointStatus.ServiceHost + ":" + fmt.Sprintf("%d", servicePort.Port)
+				}
+
+			}
+
+			return ingrtn
+		}
+	}
+	return ingrtn
+}
+
 func (r *Reconciler) Handle(ctx context.Context, event sdk.Event) error {
 	logrus.Info("handling object ", event.Object.GetObjectKind().GroupVersionKind().String())
-	// check for a fuse cr in the same namespace.
-	// if present create an integration resource
-	return nil
+
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		logrus.Fatalf("Failed to get watch namespace: %v", err)
+	}
+	hasSyndesis, err := syndesisExists(namespace)
+	if err != nil {
+		return err
+	}
+	if hasSyndesis == false {
+		return nil
+	}
+
+	configMap, ok := event.Object.(*v12.ConfigMap)
+	logrus.Info("handling address space config map ", configMap.Name, event)
+	if !ok {
+		return errors.New("expected a config map object but got " + event.Object.GetObjectKind().GroupVersionKind().String())
+	}
+
+	addressSpace, err := unMarshalAddressSpace(configMap)
+	if err !=  nil {
+		logrus.Fatalf("Failed to unmarshall addressspace data: %v", err)
+	}
+
+	ingrtn := createIntegrationResource(addressSpace, namespace)
+	if event.Deleted == true {
+		return sdk.Delete(ingrtn)
+	} else {
+		return sdk.Create(ingrtn)
+	}
 }
